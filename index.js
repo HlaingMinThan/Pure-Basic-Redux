d103b62dee2445d555e3fb86fd0b7ff492a4f468
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-03-26 23:21:00 +0630

    example using middleware in redux (redux logger

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-let { createStore, combineReducers } = require('redux');
+let { createStore, combineReducers , applyMiddleware } = require('redux');
 let { buyCake , cakeReducer } = require('./cakeReducer');
 let { buyIceCream , iceCreamReducer } = require('./iceCreamReducer');
+let { createLogger } = require('redux-logger');
+const logger = createLogger({
+  // ...options
+});
 
 let rootReducer = combineReducers({
   cake : cakeReducer,
   iceCream : iceCreamReducer
 })
 
-let store = createStore(rootReducer);
+let store = createStore(rootReducer , applyMiddleware(logger));
 console.log('Our Initial state is ',store.getState())
-let unsubscribe = store.subscribe(() => console.log('Our Updated state is ',store.getState()))
+let unsubscribe = store.subscribe(() => {})
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
